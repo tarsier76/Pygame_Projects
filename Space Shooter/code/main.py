45566b51bf0781d99cb26191eff561d5b3de23df
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-06-26 17:29:40 +0300

    Testing space bar press functionality

--- Space Shooter/code/main.py
+++ Space Shooter/code/main.py
@@ -10,8 +10,8 @@ clock = pygame.time.Clock()
 
 player_surf = pygame.image.load('../images/player.png').convert_alpha()
 player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(1,1)
-player_speed = 1000
+player_direction = pygame.math.Vector2()
+player_speed = 300
 
 star_surf = pygame.image.load('../images/star.png').convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for time in range(20)]
@@ -22,11 +22,29 @@ meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
 laser_surf = pygame.image.load('../images/laser.png').convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT- 20))
 
+spacebar_pressed = False 
+
 while running:
     dt = clock.tick() / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE] and not spacebar_pressed:
+        print("Shooting Laser!")
+        spacebar_pressed = True 
+    elif not keys[pygame.K_SPACE]:
+        spacebar_pressed = False
+    
+   
+
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction
+
+    player_rect.center += player_direction * dt * player_speed
+
     display_surface.fill(color='darkgray')
     for position in star_positions:
         display_surface.blit(star_surf, position)
@@ -34,11 +52,6 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
 
     display_surface.blit(player_surf, player_rect)
-    player_rect.center += player_direction * dt * player_speed
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction.x *= -1  
-    if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
-        player_direction.y *= -1  
     
     pygame.display.update()
 
