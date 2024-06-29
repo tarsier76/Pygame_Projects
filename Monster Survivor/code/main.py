e453bda9e62f33874bb07a60f86ae6ac6a4a678b
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-06-29 19:07:42 +0300

    Imported map and collision objects into the game

--- Monster Survivor/code/main.py
+++ Monster Survivor/code/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from sprites import * 
 from random import randint 
+from pytmx.util_pygame import load_pygame
 
 class Game:
     def __init__(self):
@@ -12,13 +13,20 @@ class Game:
         self.clock = pygame.time.Clock()
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
+        self.setup()
         self.player = Player(self.all_sprites, (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), self.collision_sprites)
 
-        for sprite in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60, 100), randint(50, 100)
-            CollisionSprite((x,y), (w, h), (self.all_sprites, self.collision_sprites))
         
+
+    def setup(self):
+        map = load_pygame('../data/maps/world.tmx')
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE ,y * TILE_SIZE), image, self.all_sprites)
+
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
+    
     def run(self):
         while self.running:
             dt = self.clock.tick() / 1000
