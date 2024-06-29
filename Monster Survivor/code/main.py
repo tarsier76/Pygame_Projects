12525ff0e986ca825335a1f030ad0b96f2aac909
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-06-29 17:16:28 +0300

    Added CollisionSprite class and collision

--- Monster Survivor/code/main.py
+++ Monster Survivor/code/main.py
@@ -1,5 +1,7 @@
 from settings import * 
 from player import Player
+from sprites import * 
+from random import randint 
 
 class Game:
     def __init__(self):
@@ -9,7 +11,13 @@ class Game:
         self.running = True 
         self.clock = pygame.time.Clock()
         self.all_sprites = pygame.sprite.Group()
-        self.player = Player(self.all_sprites, (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.collision_sprites = pygame.sprite.Group()
+        self.player = Player(self.all_sprites, (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), self.collision_sprites)
+
+        for sprite in range(6):
+            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
+            w, h = randint(60, 100), randint(50, 100)
+            CollisionSprite((x,y), (w, h), (self.all_sprites, self.collision_sprites))
         
     def run(self):
         while self.running:
@@ -18,7 +26,7 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            self.display.fill('gray')
+            self.display.fill('black')
             
             self.all_sprites.update(dt)
             self.all_sprites.draw(self.display)
--- Monster Survivor/code/player.py
+++ Monster Survivor/code/player.py
@@ -1,12 +1,15 @@
 from settings import * 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, group, pos):
+    def __init__(self, group, pos, collision_sprites):
         super().__init__(group)
         self.image = pygame.image.load('../images/player/down/0.png').convert_alpha()
         self.rect = self.image.get_frect(center=pos)
+        self.hitbox_rect = self.rect.inflate(-60,0)
+
         self.direction = pygame.math.Vector2()
         self.speed = 400
+        self.collision_sprites = collision_sprites
 
     def input(self):
         pressed_button = pygame.key.get_pressed()
@@ -15,10 +18,24 @@ class Player(pygame.sprite.Sprite):
         self.direction = self.direction.normalize() if self.direction else self.direction
 
     def move(self, dt):
-        self.rect.center += self.speed * dt * self.direction
+        self.hitbox_rect.x += self.speed * dt * self.direction.x
+        self.collision('horizontal')
+        self.hitbox_rect.y += self.speed * dt * self.direction.y
+        self.collision('vertical')
+        self.rect.center = self.hitbox_rect.center
+
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+                else:
+                    if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top 
+                    if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
 
     def update(self, dt):
         self.input()
         self.move(dt)
         
-        
+    
