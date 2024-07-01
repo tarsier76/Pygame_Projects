61ec4c8042e789f5cfc86bc7608ff682a1286aea
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-01 08:27:40 +0300

    Fixed gun rotation angle

--- Monster Survivor/code/sprites.py
+++ Monster Survivor/code/sprites.py
@@ -18,7 +18,7 @@ class Gun(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         self.player = player 
         self.distance = 140 
-        self.player_direction = pygame.Vector2(0,0)
+        self.player_direction = pygame.Vector2(1,0)
 
         super().__init__(groups)
         self.gun_surf = pygame.image.load('../images/gun/gun.png').convert_alpha()
@@ -32,9 +32,17 @@ class Gun(pygame.sprite.Sprite):
 
     def rotate_gun(self):
         angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90 
-        self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        print(self.player_direction.x, self.player_direction.y)
+        print(angle)
+        if self.player_direction.x > 0:
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image, False, True)
 
     def update(self, _):
         self.get_direction()
         self.rotate_gun()
         self.rect.center = self.player.rect.center + self.player_direction * self.distance
+
+
